46c6dc5cc9814aaf8cef00b08342bc86046ac2e5
Adam McMahon <obelesk411@users.noreply.github.com>
2016-11-05 08:55:56 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, function(err) {
+	if(err) throw err;
+
+	else console.log('Up and running on port 3000');
+});
