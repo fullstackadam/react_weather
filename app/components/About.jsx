5b315377edbfa92f65d016ec844b5643709c63b3
Adam McMahon <obelesk411@users.noreply.github.com>
2016-11-06 17:35:11 -0700

    feat: add content and styles to about

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,8 +1,16 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = (props) => {
 	return (
-		<h3>About Component</h3>
+		<div>
+			<h1 className="text-center">About</h1>
+			<p className="text-center">
+				Built with <span style={{color: 'red'}}>&hearts;</span> using <Link to='https://foundation.zurb.com/'>Foundation</Link>, 
+				<Link to='https://facebook.github.io/react/'> React </Link> 
+				and <br/><Link to='https://openweathermap.org/'>Open Weather Map</Link>
+			</p>
+		</div>
 	);
 };
 
