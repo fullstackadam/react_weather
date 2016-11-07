2acedf7b046391a03b18551f661ca59795d4b58e
Adam McMahon <obelesk411@users.noreply.github.com>
2016-11-06 17:44:25 -0700

    feat: style weather components

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var WeatherMessage = ({temp, city}) => {
 	return (
 		<div>
-			<h2>It is {temp} in {city}</h2>
+			<h3 className="text-center">It is {temp} in {city}</h3>
 		</div>
 	);
 };
